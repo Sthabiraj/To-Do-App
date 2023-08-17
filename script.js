e9be52dfbfee551268b395b2bc558686193c87de
Sthabiraj <birajshrestha2007@gmail.com>
2023-08-17 11:04:12 +0545

    js updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (inputBox == "") {
+  if (inputBox.value == "") {
     alert("You must write something.");
   } else {
     const li = document.createElement("li");
